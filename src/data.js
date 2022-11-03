f770f967c828a2101df4f25b5d1d1feec736a14b
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-11-03 21:06:43 +0100

    Vytvoril som základnú app hellov-robots

---
+++ src/data.js
@@ -0,0 +1,103 @@
+const data = [
+
+    {
+    id:1,
+    first_name:"Lorine",
+    last_name:"Gove",
+    email:"lgove0@acquirethisname.com",
+    gender:"Female",
+    category: "Good-robots",
+    Avatar:"https://robohash.org/quisdistinctionemo.jpg?size=200x200&set=set1"
+    },
+
+    {
+    id:2,first_name:"Alida",
+    last_name:"Giacomi",
+    email:"agiacomi1@ow.ly",
+    gender:"Female",
+    category: "Good-robots",
+    Avatar:"https://robohash.org/repellatofficiisquia.jpg?size=200x200&set=set1"
+    },
+
+    {
+    id:3,
+    first_name:"Vassili",
+    last_name:"Eblein",
+    email:"veblein2@fotki.com",
+    gender:"Male",
+    category: "Good-robots",
+    Avatar:"https://robohash.org/voluptatumidet.jpg?size=200x200&set=set1"
+    },
+
+    {
+    id:4,
+    first_name:"Michell",
+    last_name:"Schoenleiter",
+    email:"mschoenleiter3@studiopress.com",
+    gender:"Female",
+    category: "Bad-robots",
+    Avatar:"https://robohash.org/iustoidvoluptatem.jpg?size=200x200&set=set1"
+    },
+
+    {
+    id:5,
+    first_name:"Cindee",
+    last_name:"Filip",
+    email:"cfilip4@nyu.edu",
+    gender:"Female",
+    category: "Bad-robots",
+    Avatar:"https://robohash.org/impeditnecessitatibusad.jpg?size=200x200&set=set1"
+    },
+
+    { 
+    id:6,
+    first_name:"Terri",
+    last_name:"MacQuist",
+    email:"tmacquist5@delicious.com",
+    gender:"Male",
+    category: "Bad-robots",
+    Avatar:"https://robohash.org/sediuresoluta.jpg?size=200x200&set=set1"
+    },
+
+    {
+    id:7,
+    first_name:"Engelbert",
+    last_name:"Wimmer",
+    email:"ewimmer6@360.cn",
+    gender:"Male",
+    category: "Neutra-robots",
+    Avatar:"https://robohash.org/etconsequaturrecusandae.jpg?size=200x200&set=set1"
+    },
+
+    {
+    id:8,
+    first_name:"Emmanuel",
+    last_name:"Mattersley",
+    email:"emattersley7@huffingtonpost.com",
+    gender:"Male",
+    category: "Neutra-robots",
+    Avatar:"https://robohash.org/autnatusperferendis.jpg?size=200x200&set=set1"
+    },
+
+    {
+        id:9,
+    first_name:"Denna",
+    last_name:"Trillo",
+    email:"dtrillo8@twitpic.com",
+    gender:"Female",
+    category: "Neutra-robots",
+    Avatar:"https://robohash.org/nihilquiquo.jpg?size=200x200&set=set1"
+    },
+
+    {
+    id:10,
+    first_name:"Silvana",
+    last_name:"Pluvier",
+    email:"spluvier9@ted.com",
+    gender:"Female",
+    category: "Alone-robot",
+    Avatar:"https://robohash.org/architectonemoaccusantium.jpg?size=200x200&set=set1"
+    }
+]
+
+export default data;
