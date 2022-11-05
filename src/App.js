4c76952365777755ba27b72f5abf784b2070ed2b
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-11-05 09:12:22 +0100

    Naimportoval som <MyList />

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.scss';
+import Header from './component/Header/Header';
+import MyList from './component/MyList/MyList';
 
 function App() {
   return (
     <main>
-
+      <Header />
+      <MyList />
     </main>
   );
 }
