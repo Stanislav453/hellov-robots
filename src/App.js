5c0db38c2349c719640b2c7f97ba78b5580e77a8
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-11-05 13:56:40 +0100

    Pridal som argument do <Mylist />

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.scss';
 import Header from './component/Header/Header';
 import MyList from './component/MyList/MyList';
+import data from './data';
 
 function App() {
   return (
     <main>
       <Header />
-      <MyList />
+      <MyList dataForList={data} />
     </main>
   );
 }
