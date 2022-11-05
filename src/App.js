1562241b73f2c75a94f7ed739038e31afb55188c
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-11-05 15:51:45 +0100

    importoval som useState pridal som onlyCategory[] Mycategories a funkciu filterCategory.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { useState } from 'react';
 import './App.scss';
 import Header from './component/Header/Header';
 import MyList from './component/MyList/MyList';
 import data from './data';
 
+const onlyCategory = ["All", ...new Set(data.map((item) => item.category))];
+
 function App() {
+
+  const [ dataitems, setDataItems ] = useState(data);
+  const [ MyCategories, setMyCategories] = useState(onlyCategory);
+
+  const filterCategory = ( categories ) => {
+
+    if(categories === "All") {
+      return setDataItems(data)
+    }
+
+    const newCategory = data.filter( (item) =>  item.category === categories )
+
+    setDataItems(newCategory)
+  }
+
+
   return (
     <main>
-      <Header />
-      <MyList dataForList={data} />
+      <Header headerOnlyCategory={onlyCategory} myfunctionHeader={filterCategory} />
+      <MyList dataForList={dataitems} />
     </main>
   );
 }
